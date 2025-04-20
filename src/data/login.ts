7627ae3baf2902e4af88b351d18f4bdc97386794
j1munoz <jimmymunoz397@gmail.com>
2025-04-20 03:38:13 -0700

    Login

---
+++ src/data/login.ts
@@ -0,0 +1,14 @@
+const login = [
+    {
+        type: "email",
+        label: "Email",
+        name: "email",
+    },
+    {
+        type: "password",
+        label: "Password",
+        name: "password",
+    },
+]
+
+export default login;
