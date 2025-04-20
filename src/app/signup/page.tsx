14f89f320bb20f867ba455cefa9bbbef50c0b4c2
j1munoz <jimmymunoz397@gmail.com>
2025-04-20 03:03:52 -0700

    Signup Page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import SignupForm from "@/components/signup/signupForm";
+
+const SignupPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Signup Page</h1>
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
---
+++ src/data/signup.ts
@@ -0,0 +1,40 @@
+const signup = [
+  {
+    name: "firstName",
+    label: "First Name",
+    type: "text",
+  },
+  {
+    name: "lastName",
+    label: "Last Name",
+    type: "text",
+  },
+  {
+    name: "major",
+    label: "Major",
+    options: [
+      "Computer Science",
+      "Mathematics",
+      "Physics",
+      "Chemistry",
+      "Biology",
+    ],
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    name: "confirmPassword",
+    label: "Confirm Password",
+    type: "password",
+  },
+];
+
+export default signup;
